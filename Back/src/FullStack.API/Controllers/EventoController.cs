6498f209130b36638657f9045f6614001c6ad34a
dionathanfreitas <49460751+dionathanfreitas@users.noreply.github.com>
2022-07-03 11:44:43 -0300

    Adicionando Elementos do EF com SQLite

--- Back/src/FullStack.API/Controllers/EventoController.cs
+++ Back/src/FullStack.API/Controllers/EventoController.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FullStack.API.Data;
 using FullStack.API.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 
 namespace FullStack.API.Controllers
@@ -12,63 +14,42 @@ namespace FullStack.API.Controllers
     [Route("api/[controller]")]
     public class EventoController : ControllerBase
     {
-        public EventoController()
+        private readonly DataContext _context;
+        public EventoController(DataContext context)
         {
+            _context = context;
 
         }
-        
-        public IEnumerable<Evento> _evento = new Evento[]
-        {
-            new Evento(){
-               EventoID = 1,
-               Tema = "FullStack com .Net + Angular",
-               Local = "Florianópolis - SC",
-               Lote = "1° Lote",
-               QtdPessoas = 300,
-               DataEvento = DateTime.Now.AddDays(2).ToString("dd/mm/yyyy"),
-               ImagemURL = "foto.png"
 
-           },
-           new Evento(){
-               EventoID = 2,
-               Tema = "FullStack o caminho da assertividade",
-               Local = "São Paulo - SP",
-               Lote = "2° Lote",
-               QtdPessoas = 500,
-               DataEvento = DateTime.Now.AddDays(3).ToString("dd/mm/yyyy"),
-               ImagemURL = "foto2.png"
-
-           }
-        };
 
         [HttpGet]
         public IEnumerable<Evento> Get()
         {
-           return _evento;
+            return _context.Eventos;
         }
 
-        [HttpGet ("{id}")]
-        public IEnumerable<Evento> GetById(int id)
+        [HttpGet("{id}")]
+        public Evento GetById(int id)
         {
-           return _evento.Where(_evento =>_evento.EventoID == id );
+            return _context.Eventos.FirstOrDefault(_evento => _evento.EventoID == id);
         }
 
         [HttpPost]
         public string Post()
         {
-           return "Exemplo de Post";
+            return "Exemplo de Post";
         }
 
-         [HttpPut("{id}")]
+        [HttpPut("{id}")]
         public string Put(int id)
         {
-           return $"Exemplo de Put com id = {id}";
+            return $"Exemplo de Put com id = {id}";
         }
 
-          [HttpDelete("{id}")]
+        [HttpDelete("{id}")]
         public string Delete(int id)
         {
-           return $"Exemplo de Delete com id = {id}";
+            return $"Exemplo de Delete com id = {id}";
         }
     }
 }
